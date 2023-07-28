a64183d17568ed57bf9e8775cc7eaf16212b5438
dawnberdan <140385784+dawnberdan@users.noreply.github.com>
2023-07-28 11:33:17 +0800

    Update streamlit_app1.py
    typo error

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -44,7 +44,7 @@ except URLError as e:
     
 #import snowflake.connector
 #streamlit.text ("The fruit load list contains:")
-streamlit.theader ("View Our Fruit List - Add Your Favorites!")
+streamlit.header ("View Our Fruit List - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
